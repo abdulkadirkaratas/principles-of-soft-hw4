7cec1ff1c73af688366cc38ddd5abee6bf8183f2
Abdulkadir Karataş <122316106+abdulkadirkaratas@users.noreply.github.com>
2024-01-11 22:37:40 +0300

    Add files via upload
    satisfying requirement one

---
+++ NumberRangePrinter.java
@@ -0,0 +1,25 @@
+package numberrangeprinter;
+
+/**
+ *
+ * @author abdulkadirkaratas
+ */
+import java.util.Scanner;
+
+public class NumberRangePrinter {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter the first number: ");
+        int firstNumber = input.nextInt();
+
+        System.out.print("Enter the second number: ");
+        int secondNumber = input.nextInt();
+
+        System.out.println("Numbers between " + firstNumber + " and " + secondNumber + " (exclusive):");
+        for (int i = firstNumber + 1; i < secondNumber; i++) {
+            System.out.print(i + " ");
+        }
+    }
+}
